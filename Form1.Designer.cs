afe956753ac3cdc1c9c3580db1da1b0cba8d1e16
PavelVorobyev77 <pascha27_05@mail.ru>
2024-03-28 11:37:26 +0700

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace _2._9Matrix_VP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.buttonMatrices1 = new System.Windows.Forms.Button();
+            this.buttonMatrices2 = new System.Windows.Forms.Button();
+            this.buttonFold = new System.Windows.Forms.Button();
+            this.buttonMultiply = new System.Windows.Forms.Button();
+            this.buttonSubtract = new System.Windows.Forms.Button();
+            this.buttonTransposition = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radiobtn1 = new System.Windows.Forms.RadioButton();
+            this.radiobtn2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(40, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(483, 187);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(812, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(483, 187);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(1458, 57);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(483, 187);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // buttonMatrices1
+            // 
+            this.buttonMatrices1.Location = new System.Drawing.Point(40, 286);
+            this.buttonMatrices1.Name = "buttonMatrices1";
+            this.buttonMatrices1.Size = new System.Drawing.Size(144, 45);
+            this.buttonMatrices1.TabIndex = 3;
+            this.buttonMatrices1.Text = "Сгенерировать 1-ю матрицу";
+            this.buttonMatrices1.UseVisualStyleBackColor = true;
+            this.buttonMatrices1.Click += new System.EventHandler(this.buttonMatrices1_Click);
+            // 
+            // buttonMatrices2
+            // 
+            this.buttonMatrices2.Location = new System.Drawing.Point(812, 286);
+            this.buttonMatrices2.Name = "buttonMatrices2";
+            this.buttonMatrices2.Size = new System.Drawing.Size(144, 45);
+            this.buttonMatrices2.TabIndex = 4;
+            this.buttonMatrices2.Text = "Сгенерировать 2-ю матрицу";
+            this.buttonMatrices2.UseVisualStyleBackColor = true;
+            this.buttonMatrices2.Click += new System.EventHandler(this.buttonMatrices2_Click);
+            // 
+            // buttonFold
+            // 
+            this.buttonFold.Location = new System.Drawing.Point(604, 57);
+            this.buttonFold.Name = "buttonFold";
+            this.buttonFold.Size = new System.Drawing.Size(114, 39);
+            this.buttonFold.TabIndex = 5;
+            this.buttonFold.Text = "Сложение(+)";
+            this.buttonFold.UseVisualStyleBackColor = true;
+            this.buttonFold.Click += new System.EventHandler(this.buttonFold_Click);
+            // 
+            // buttonMultiply
+            // 
+            this.buttonMultiply.Location = new System.Drawing.Point(604, 113);
+            this.buttonMultiply.Name = "buttonMultiply";
+            this.buttonMultiply.Size = new System.Drawing.Size(114, 39);
+            this.buttonMultiply.TabIndex = 6;
+            this.buttonMultiply.Text = "Умножение (*)";
+            this.buttonMultiply.UseVisualStyleBackColor = true;
+            this.buttonMultiply.Click += new System.EventHandler(this.buttonMultiply_Click);
+            // 
+            // buttonSubtract
+            // 
+            this.buttonSubtract.Location = new System.Drawing.Point(604, 167);
+            this.buttonSubtract.Name = "buttonSubtract";
+            this.buttonSubtract.Size = new System.Drawing.Size(114, 39);
+            this.buttonSubtract.TabIndex = 7;
+            this.buttonSubtract.Text = "Вычитание (-)";
+            this.buttonSubtract.UseVisualStyleBackColor = true;
+            this.buttonSubtract.Click += new System.EventHandler(this.buttonSubtract_Click);
+            // 
+            // buttonTransposition
+            // 
+            this.buttonTransposition.Location = new System.Drawing.Point(592, 223);
+            this.buttonTransposition.Name = "buttonTransposition";
+            this.buttonTransposition.Size = new System.Drawing.Size(146, 39);
+            this.buttonTransposition.TabIndex = 8;
+            this.buttonTransposition.Text = "Транспонирование";
+            this.buttonTransposition.UseVisualStyleBackColor = true;
+            this.buttonTransposition.Click += new System.EventHandler(this.buttonTransposition_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Матрица 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(809, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Матрица 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1455, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Результат";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1355, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 38);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "=";
+            // 
+            // radiobtn1
+            // 
+            this.radiobtn1.AutoSize = true;
+            this.radiobtn1.Location = new System.Drawing.Point(40, 361);
+            this.radiobtn1.Name = "radiobtn1";
+            this.radiobtn1.Size = new System.Drawing.Size(215, 20);
+            this.radiobtn1.TabIndex = 13;
+            this.radiobtn1.TabStop = true;
+            this.radiobtn1.Text = "Транспонировать матрицу 1";
+            this.radiobtn1.UseVisualStyleBackColor = true;
+            // 
+            // radiobtn2
+            // 
+            this.radiobtn2.AutoSize = true;
+            this.radiobtn2.Location = new System.Drawing.Point(812, 361);
+            this.radiobtn2.Name = "radiobtn2";
+            this.radiobtn2.Size = new System.Drawing.Size(215, 20);
+            this.radiobtn2.TabIndex = 14;
+            this.radiobtn2.TabStop = true;
+            this.radiobtn2.Text = "Транспонировать матрицу 2";
+            this.radiobtn2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2033, 793);
+            this.Controls.Add(this.radiobtn2);
+            this.Controls.Add(this.radiobtn1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonTransposition);
+            this.Controls.Add(this.buttonSubtract);
+            this.Controls.Add(this.buttonMultiply);
+            this.Controls.Add(this.buttonFold);
+            this.Controls.Add(this.buttonMatrices2);
+            this.Controls.Add(this.buttonMatrices1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button buttonMatrices1;
+        private System.Windows.Forms.Button buttonMatrices2;
+        private System.Windows.Forms.Button buttonFold;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button buttonSubtract;
+        private System.Windows.Forms.Button buttonTransposition;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RadioButton radiobtn1;
+        private System.Windows.Forms.RadioButton radiobtn2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._9Matrix_VP
+{
+    public partial class Form1 : Form
+    {
+        private int[,] MultiplyMatrices(int[,] matrix1, int[,] matrix2)
+        {
+            int rows1 = matrix1.GetLength(0);
+            int cols1 = matrix1.GetLength(1);
+            int cols2 = matrix2.GetLength(1);
+
+            int[,] result = new int[rows1, cols2];
+
+            for (int i = 0; i < rows1; i++)
+            {
+                for (int j = 0; j < cols2; j++)
+                {
+                    int sum = 0;
+                    for (int k = 0; k < cols1; k++)
+                    {
+                        sum += matrix1[i, k] * matrix2[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+
+            return result;
+        }
+
+        private int[,] TransposeMatrix(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+
+            int[,] transposedMatrix = new int[cols, rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    transposedMatrix[j, i] = matrix[i, j];
+                }
+            }
+
+            return transposedMatrix;
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonFold_Click(object sender, EventArgs e)
+        {
+            int[,] matrix1 = GetMatrixFromDataGridView(dataGridView1);
+            int[,] matrix2 = GetMatrixFromDataGridView(dataGridView2);
+
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                MessageBox.Show("Невозможно сложить/вычесть матрицы с разными размерами.");
+                return;
+            }
+
+            int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    // Выполняем сложение 
+                    result[i, j] = matrix1[i, j] + matrix2[i, j]; // Сложение
+
+                }
+            }
+
+            DisplayMatrixInDataGridView(result, dataGridView3);
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            int[,] matrix1 = GetMatrixFromDataGridView(dataGridView1);
+            int[,] matrix2 = GetMatrixFromDataGridView(dataGridView2);
+
+            if (matrix1.GetLength(1) == matrix2.GetLength(0))
+            {
+                MessageBox.Show("Невозможно умножить матрицы с данными размерами.");
+                return;
+            }
+
+            int[,] result = MultiplyMatrices(matrix1, matrix2);
+            DisplayMatrixInDataGridView(result, dataGridView3);
+        }
+
+        private void buttonSubtract_Click(object sender, EventArgs e)
+        {
+            int[,] matrix1 = GetMatrixFromDataGridView(dataGridView1);
+            int[,] matrix2 = GetMatrixFromDataGridView(dataGridView2);
+
+            if (matrix1.GetLength(0) != matrix2.GetLength(0) || matrix1.GetLength(1) != matrix2.GetLength(1))
+            {
+                MessageBox.Show("Невозможно сложить/вычесть матрицы с разными размерами.");
+                return;
+            }
+
+            int[,] result = new int[matrix1.GetLength(0), matrix1.GetLength(1)];
+            for (int i = 0; i < matrix1.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix1.GetLength(1); j++)
+                {
+                    // Выполняем вычитание
+                    result[i, j] = matrix1[i, j] - matrix2[i, j]; // Вычитание
+                }
+            }
+
+            DisplayMatrixInDataGridView(result, dataGridView3);
+        }
+
+        private void buttonTransposition_Click(object sender, EventArgs e)
+        {
+            if (radiobtn1.Checked == true)
+            {
+                int[,] matrix = GetMatrixFromDataGridView(dataGridView1);
+                int[,] transposedMatrix = TransposeMatrix(matrix);
+                DisplayMatrixInDataGridView(transposedMatrix, dataGridView3);
+            }
+            else
+            {
+                int[,] matrix = GetMatrixFromDataGridView(dataGridView2);
+                int[,] transposedMatrix = TransposeMatrix(matrix);
+                DisplayMatrixInDataGridView(transposedMatrix, dataGridView3);
+            }
+        }
+
+        private void buttonMatrices1_Click(object sender, EventArgs e)
+        {
+            int[,] matrix1 = GenerateRandomMatrix(3, 3);
+            DisplayMatrixInDataGridView(matrix1, dataGridView1);
+        }
+
+        private void buttonMatrices2_Click(object sender, EventArgs e)
+        {
+            int[,] matrix2 = GenerateRandomMatrix(3, 3);
+            DisplayMatrixInDataGridView(matrix2, dataGridView2);
+        }
+
+        //генерация значений матриц.
+        private int[,] GenerateRandomMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+            Random random = new Random();
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = random.Next(1, 10); // Генерация случайного числа от 1 до 10
+                }
+            }
+
+            return matrix;
+        }
+
+        //Отображение матриц в DataGridView 
+        private void DisplayMatrixInDataGridView(int[,] matrix, DataGridView dataGridView)
+        {
+            try
+            {
+                dataGridView.Rows.Clear();
+                dataGridView.Columns.Clear();
+
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    dataGridView.Columns.Add("", "");
+                }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    DataGridViewRow row = new DataGridViewRow();
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        row.Cells.Add(new DataGridViewTextBoxCell { Value = matrix[i, j] });
+                    }
+                    dataGridView.Rows.Add(row);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private int[,] GetMatrixFromDataGridView(DataGridView dataGridView)
+        {
+            int rows = dataGridView.Rows.Count;
+            int cols = dataGridView.Columns.Count;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(dataGridView.Rows[i].Cells[j].Value);
+                }
+            }
+            return matrix;
+        }
+    }
+}
+
